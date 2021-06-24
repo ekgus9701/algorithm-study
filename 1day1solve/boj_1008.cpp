1637ebd5e6de4ed5cc653115af7c1ddfa8bff1af
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-06-24 22:39:51 +0900

    0624

--- 1day1solve/boj_1008.cpp
+++ 1day1solve/boj_1008.cpp
@@ -7,8 +7,8 @@ int main()
     double a, b;
     cin >> a >> b;
 
-    cout.precision(12);
-
+    cout.precision(12); //전체 12자리를 표현하겠다.
+    cout << fixed;      //fixed를 쓰면 소수점 아래 12자리를 표현하겠다는 뜻
     cout << a / b;
     return 0;
 }
