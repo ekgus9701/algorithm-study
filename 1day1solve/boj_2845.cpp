295f421433540677888b7d51d1a2257e768e6b63
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-07-14 15:52:22 +0900

    0714

---
+++ 1day1solve/boj_2845.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int l, p,k;
+    int num[5];
+
+    cin >> l>>p;
+    k = l * p;
+    for (int i = 0; i < 5;i++){
+        cin >> num[i];
+    }
+
+    for (int i = 0; i < 5;i++){
+        cout << num[i] - k << " ";
+    }
+}
