06a7abd41d5650d4e7f7245bd911968feb341d9e
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-07-28 01:05:18 +0900

    0728

---
+++ 1day1solve/boj_5596.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int mk[4], ms[4];
+    int sum1=0, sum2=0;
+    for (int i = 0; i < 4;i++){
+        cin >> mk[i];
+        sum1 += mk[i];
+    }
+    for (int i = 0; i < 4;i++){
+        cin >> ms[i];
+        sum2 += ms[i];
+        
+    }
+
+    if(sum1<sum2){
+        cout << sum2;
+    }
+    else if(sum1==sum2)
+        cout << sum1;
+    else{
+        cout << sum1;
+    }
+}
