50b494db6d4ed7b7061bd45b20d9b19be6258cba
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-08-18 11:46:45 +0900

    0818

---
+++ 1day1solve/boj_11719.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string s;
+    while(true){
+        getline(cin, s);
+        if(cin.eof()==true)
+            break;
+        cout << s<<endl;
+    }
+}
