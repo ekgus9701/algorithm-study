a05e70de21bfa5623fe0678bb8de3205f782ee96
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2021-09-06 18:05:47 +0900

    0906

---
+++ 1day1solve/Epper14_7.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+vector<string> solution(int r, int c, int zr, int zc, vector<string> words)
+{
+    vector<string> answer;
+    string temp; 
+    
+    for (int i = 0; i < r; i++)
+    { 
+        temp = "";
+        for (int j = 0; j < c; j++)
+        { 
+            for (int k = 0; k < zc; k++)
+            {  
+                temp += words[i][j];
+            }
+        }
+        for (int l = 0; l < zr; l++)
+        {   
+            answer.push_back(temp);
+        }
+    }
+    return answer;
+}
+
+int main()
+{
+    int r, c, zr, zc;
+    vector<string> answer, words;
+    string temp;
+
+    cin >> r >> c >> zr >> zc;
+
+    for (int i = 0; i < r; i++)
+    {
+        cin >> temp;
+
+        if (temp.length() > c)
+        {
+            cout << "입력 범위를 초과하였습니다.\n";
+            exit(1);
+        }
+        words.push_back(temp);
+    }
+    for (int i = 0; i < words.size(); i++)
+    {
+        cout << words[i];
+    }
+
+    answer = solution(r, c, zr, zc, words);
+
+    for (auto i : answer)
+    {
+        cout << i << endl;
+    }
+
+    return 0;
+}
