ca380fec76879c155245849333c9c46217457450
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2023-07-23 17:14:22 +0900

    0723

---
+++ 1day1solve/cpp/pgr_자연수 뒤집어 배열로 만들기.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    
+    while(true){
+        if(n==0){
+            break;
+        }
+        
+        answer.push_back(n%10);
+        n/=10;
+    }
+    
+    
+    return answer;
+}
