1414162012df8cd11d6a222827ef4503ec678a82
Dahyeon Han <52192706+ekgus9701@users.noreply.github.com>
2023-09-09 02:06:45 +0900

    230909/ 구현

---
+++ 1day1solve/cpp/pgr_마법의 엘리베이터.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int storey) {
+    int answer = 0;
+    int cnt=0;
+    int mok=storey,namoji;
+    
+    while(true){
+        if(mok<=0) break; 
+        namoji=mok%10;
+        
+        mok/=10; // 뒤에서 써야해서 몫을 미리 나눠놓음
+        
+        //나머지가 5미만, 5일때, 5초과일때로 경우를 나눔 
+        
+        if(namoji<5){ 
+            cnt+=namoji; //나머지번만큼 가면 되기 때문에 나머지를 cnt에 더한다.
+            
+        }
+        
+        else if(namoji==5){
+           
+            if(mok%10==9||mok%10==5){ //95의 경우 5를 더하는게 나음 , 5555의 경우 5를 더하는게 나음
+                cnt+=10-namoji;
+                mok+=1;
+            }
+           
+            else{ 
+                cnt+=namoji;
+            }
+        }
+        
+        else{//5보다 크면 
+            cnt+=10-namoji; 
+            mok+=1; //앞자리수 1개를 올려줌
+        }
+        
+    }
+    return cnt;
+}
