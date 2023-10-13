43eeb41fca7c662d5b502e64470b63fdb7e0ee25
ekgus9701 <52192706+ekgus9701@users.noreply.github.com>
2023-10-13 22:19:03 +0900

    231013/DP

---
+++ 1day1solve/cpp/pgr_숫자변환하기.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+#include<climits>
+#include<vector>
+
+using namespace std;
+
+
+
+int solution(int x, int y, int n) {
+    int answer=0;
+    int dp[1000001];
+    for(int i=0;i<1000001;i++){
+        dp[i]=1000001;
+    }
+    
+    dp[y]=0;
+    
+    for(int i=y;i>=x;i--){
+        if(i%3==0){
+            dp[i/3]=min(dp[i/3],dp[i]+1);
+            
+        }
+        if(i%2==0){
+             dp[i/2]=min(dp[i/2],dp[i]+1);
+        }
+        if(i-n>0)
+            dp[i-n]=min(dp[i-n],dp[i]+1);
+    }
+    
+    if(dp[x]==1000001) return -1;
+    
+    return dp[x];
+}
