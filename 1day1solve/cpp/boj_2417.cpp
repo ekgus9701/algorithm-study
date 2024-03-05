e171a8b7002e6f34d80651c342747ff4d2ff93bf
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-03-05 19:13:52 +0900

    240305 / 수학

--- 1day1solve/cpp/boj_2417.cpp
+++ 1day1solve/cpp/boj_2417.cpp
@@ -1,30 +1,17 @@
 #include <iostream>
 #include<vector>
 #include<algorithm>
+#include<cmath>
 
 using namespace std;
 
 int main(){
-    unsigned long long n,min=LLONG_MAX;
-    unsigned long long left=0,right,mid;
+    long long n;
     cin>>n;
-    right=n;
-    while(left<=right){
-        mid=(right+left)/2;
-        if(mid*mid<n){
-            left=mid+1;
-        }
-        else if(mid*mid==n){
-            cout<<mid;
-            return 0;
-        }
-        else{
-            min=mid;
-            right=mid-1;
-            
-        }
-        //cout<<left<<mid<<right<<"\n";
-        
-    }
-    cout<<min;
+    long double num;
+    num=sqrt(n);
+    cout<<static_cast<long long>(floor(num))<<endl;
+  
+
+    //cout<<min;
 }
